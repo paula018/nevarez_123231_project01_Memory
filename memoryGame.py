3efc372acfe0d173491b22a7abe3ecec1ff31431
paula018 <82911400+paula018@users.noreply.github.com>
2021-05-12 21:40:32 -0400

    Update memoryGame.py

--- memoryGame.py
+++ memoryGame.py
@@ -2,23 +2,67 @@ from graphics import *
 import random
 import time 
 
+# This function contains the instructions of the game
+def gameInstructions(memoryGameInstructions, win):
+    title = Text(Point(1.5, 0.5), "MEMORY GAME")
+    title.setSize(36)
+    title.setStyle("bold")
+    title.draw(win)
+    memoryGameInstructions.append(title)
+    
+    inst1 = Text(Point(1.5, 1), "Memorize the order in which the squares ligth up.")
+    inst1.setSize(16)
+    inst1.setStyle("bold")
+    inst1.draw(win)
+    memoryGameInstructions.append(inst1)
+    
+    inst2 = Text(Point(1.5, 1.2), "Then, click them in the correct order. If you ")
+    inst2.setSize(16)
+    inst2.setStyle("bold")
+    inst2.draw(win)
+    memoryGameInstructions.append(inst2)
+    
+    inst3 = Text(Point(1.5, 1.4), "select them in the rigth order in all 5 rounds you ")
+    inst3.setSize(16)
+    inst3.setStyle("bold")
+    inst3.draw(win)
+    memoryGameInstructions.append(inst3)
+    
+    inst4 = Text(Point(1.5, 1.6), "you win.")
+    inst4.setSize(16)
+    inst4.setStyle("bold")
+    inst4.draw(win)
+    memoryGameInstructions.append(inst4)
+    
+    start = Rectangle(Point(1.1, 2), Point(1.9, 2.2))
+    start.setWidth(3)
+    start.draw(win)
+    memoryGameInstructions.append(start)
+    
+    startLabel = Text(Point(1.5, 2.1), "LET'S START!")
+    startLabel.setSize(16)
+    startLabel.setStyle("bold")
+    startLabel.draw(win)
+    memoryGameInstructions.append(startLabel)
+
+
 # This function contains the grid of the game
-def gameGrid(memoryGame, win):
+def gameGrid(memoryGameGrid, win):
     vline1 = Line(Point(1,0), Point(1,3))
     vline1.draw(win)
-    memoryGame.append(vline1)
+    memoryGameGrid.append(vline1)
     
     vline2 = Line(Point(2,0), Point(2,3))
     vline2.draw(win)
-    memoryGame.append(vline2)
+    memoryGameGrid.append(vline2)
     
     hline1 = Line(Point(0,1), Point(3,1))
     hline1.draw(win)
-    memoryGame.append(hline1)
+    memoryGameGrid.append(hline1)
         
     hline2 = Line(Point(0,2), Point(3,2))
     hline2.draw(win)
-    memoryGame.append(hline2)
+    memoryGameGrid.append(hline2)
 
 # This function generates a square with random points
 def randomSquare(win, x1, y1, x2, y2):
@@ -30,14 +74,14 @@ def randomSquare(win, x1, y1, x2, y2):
     time.sleep(.5)
 
 # This function verifies if he clicked the correct square
-def clickDetector(click, x1, y1, x2, y2, win, memoryGame):
+def clickDetector(click, x1, y1, x2, y2, win, memoryGameGrid):
     square = Rectangle(Point(x1, y1), Point(x2, y2)) 
     
     # If he clicks the rigth square it will turn white 
     if(click.getX() > x1 and click.getX() < x2 and click.getY() > y1 and click.getY() < y2):
         square.setFill("yellow")
         square.draw(win)
-        time.sleep(1)
+        time.sleep(0.5)
         square.undraw()
         return 1
     
@@ -49,7 +93,7 @@ def clickDetector(click, x1, y1, x2, y2, win, memoryGame):
         time.sleep(1)
         square.undraw()
         
-        for mG in memoryGame:
+        for mG in memoryGameGrid:
             mG.undraw()
             
         win.setBackground("red")
@@ -136,32 +180,38 @@ def playAgainOrGameOver(win):
         win.close()
 
 def main():
-    memoryGame = []
+    memoryGameGrid = []
+    memoryGameInstructions = []
     
     # Create the graphics window and set the coordinates and background
     win = GraphWin("Memory Game",600,600)
     win.setCoords(0,3,3,0)
     win.setBackground("white")
     
-    # Create the game grid
-    gameGrid(memoryGame, win)
+    # Create the game grid when hitted start
+    gameInstructions(memoryGameInstructions, win)
+    click = win.getMouse()
+    if(click.getX() > 1.1 and click.getX() < 1.9 and click.getY() > 2 and click.getY() < 2.2):
+        for mG in memoryGameInstructions:
+            mG.undraw()
+        gameGrid(memoryGameGrid, win)
     
     # Create a for loop that contains the core of the game 
     for i in range(5):
         # Create a counter variable for the rounds won 
         counter = 0
-        
+
         # Crate variables that select random squares
         x1 = random.randrange(1,3)
         x2 = x1 - 1
         y1 = x2
         y2 = x1  
-        
+
         x3 = random.randrange(0,2)
         x4 = x3 + 1
         y3 = x4
         y4 = y3 + 1
-        
+
         x5 = 3
         x6 = 2
         y5 = random.randrange(1,3)
@@ -171,66 +221,66 @@ def main():
         x8 = x7 - 1
         y7 = x8
         y8 = x7 
-        
+
         x9 = random.randrange(0,2)
         x10 = x9 + 1
         y9 = x10
         y10 = y9 + 1
-        
+
         # Create the random squares
         if i >= 0:
             randomSquare(win, x2, y1, x1, y2)
-            
+
             if i >= 1:
                 randomSquare(win, x3, y3, x4, y4)
-                
+
                 if i >= 2:
                     randomSquare(win, x6, y6, x5, y5) 
-                    
+
                     if i >= 3:
                         randomSquare(win, x8, y7, x7, y8)
-                        
+
                         if i == 4:
                             randomSquare(win, x9, y9, x10, y10)       
-        
+
         # Verify that the player clicked the correct square in each round
         # if he did add 1 to the counter
         # if he didn't he will loose the game
         if i >= 0:
             click1 = win.getMouse()
-            if (clickDetector(click1, x2, y1, x1, y2, win, memoryGame) == 1):
+            if (clickDetector(click1, x2, y1, x1, y2, win, memoryGameGrid) == 1):
                 counter = counter + 1  
-            clickDetector(click1, x2, y1, x1, y2, win, memoryGame)      
-   
+            clickDetector(click1, x2, y1, x1, y2, win, memoryGameGrid)      
+
             if i >= 1:
                 click2 = win.getMouse()
-                if (clickDetector(click2, x3, y3, x4, y4, win, memoryGame) == 1):
+                if (clickDetector(click2, x3, y3, x4, y4, win, memoryGameGrid) == 1):
                     counter = counter + 1
-                clickDetector(click2, x3, y3, x4, y4, win, memoryGame) 
-                    
+                clickDetector(click2, x3, y3, x4, y4, win, memoryGameGrid) 
+
                 if i >= 2:
                     click3 = win.getMouse() 
-                    if (clickDetector(click3, x6, y6, x5, y5, win, memoryGame) == 1):
+                    if (clickDetector(click3, x6, y6, x5, y5, win, memoryGameGrid) == 1):
                         counter = counter + 1
-                    clickDetector(click3, x6, y6, x5, y5, win, memoryGame) 
-                        
+                    clickDetector(click3, x6, y6, x5, y5, win, memoryGameGrid) 
+
                     if i >= 3:
                         click4 = win.getMouse() 
-                        if (clickDetector(click4, x8, y7, x7, y8, win, memoryGame) == 1):
+                        if (clickDetector(click4, x8, y7, x7, y8, win, memoryGameGrid) == 1):
                             counter = counter + 1
-                        clickDetector(click4, x8, y7, x7, y8, win, memoryGame) 
-                            
+                        clickDetector(click4, x8, y7, x7, y8, win, memoryGameGrid) 
+                        
                         if i == 4:
                             click5 = win.getMouse() 
-                            if (clickDetector(click5, x9, y9, x10, y10, win, memoryGame) == 1):
+                            if (clickDetector(click5, x9, y9, x10, y10, win, memoryGameGrid) == 1):
                                 counter = counter + 1
-                            clickDetector(click5, x9, y9, x10, y10, win, memoryGame) 
-        
+                            clickDetector(click5, x9, y9, x10, y10, win, memoryGameGrid) 
+
         # If he won all the rounds, it means he won the game 
         # everything will be undraw from the screen, a winning trophy will appear
         # and he will have the option to play again or end the game
         if (counter == 5):
-            for mG in memoryGame:
+            for mG in memoryGameGrid:
                 mG.undraw()
             winningTrophy(win)
             playAgainOrGameOver(win)
